371a646ec04973bdb3bbb1d035ca9245403b3370
Aarav261 <anu0310v@gmail.com>
2025-12-09 18:29:26 +1100

    Event scraper modified to extract individual event page

--- scraper.py
+++ scraper.py
@@ -1,26 +1,60 @@
 from playwright.sync_api import sync_playwright
 import agentql
 
-QUERY = """
-{
-    events[]{
-        name
-        date
-        location
-        link}
+def main():
 
-}"""
+    QUERY = """
+    {
+        events[]{
+            name
+            date
+            location
+            link}
 
-links= []
-with sync_playwright() as playwright:
-    browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
-    agentql_page = agentql.wrap(page)
-    agentql_page.goto("https://www.meetup.com/en-AU/find/?keywords=tech&location=au--Canterbury&source=EVENTS&distance=tenMiles")
-    response = agentql_page.query_data(QUERY)
+    }"""
+
+    QUERY_DETAILS = """{
+         event_details[]{
+            title
+            date
+            description
+            location}
+    }"""
+
+    def extract_meetup_links():
+        links= []
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=False)
+            page = browser.new_page()
+            agentql_page = agentql.wrap(page)
+            agentql_page.goto("https://www.meetup.com/en-AU/find/?keywords=tech&location=au--Canterbury&source=EVENTS&distance=tenMiles")
+            response = agentql_page.query_data(QUERY)
+            
+            for link in response['events']:
+                links.append(link['link'])
+
+            browser.close()
+
+        return links
     
-    for link in response['events']:
-        links.append(link['link'])
-    print(links)
+    links = extract_meetup_links()
 
-    browser.close()
+    def extract_meetup_events(links):
+        with sync_playwright() as playwright:
+            browser = playwright.chromium.launch(headless=False)
+            
+            for link in links:
+                page = browser.new_page()
+                agentql_page = agentql.wrap(page)
+                agentql_page.goto(link)
+                response = agentql_page.query_data(QUERY_DETAILS)
+                print(response)
+
+            browser.close()
+
+    extract_meetup_events(links)
+
+if __name__ == "__main__":
+    main()
+            
+            
